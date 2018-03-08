8aa76b95fbaf15b8ea370aa5eecb1a6b81256bb5
root <root@localhost.localdomain>
2018-03-08 11:50:32 +0300

    2

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var port = process.argv[2];
     var app = express()
-    app.get('/home', function(req, res) {
-      res.end('Hello World!')
-    })
+    app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+    // app.get('/home', function(req, res) {
+    //   res.end('Hello World!')
+    // })
     app.listen(port)
